a4ed85f1620c7513e3c8ccc08a49120706081526
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-03-13 15:08:46 +0900

    styles: globals css 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
-
 export const metadata: Metadata = {
   title: "지성인",
   description: "책의 모든 것",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
