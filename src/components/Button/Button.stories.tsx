c1bf5685699a86b24f78fac597fe5aca8412604e
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-03-13 15:11:57 +0900

    feat: 버튼 컴포넌트 생성

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+  title: "components/atoms/button",
+  component: Button,
+  argTypes: {
+    variant: {
+      control: "inline-radio",
+      options: ["main", "empty"],
+    },
+    rounded: {
+      control: "inline-radio",
+      options: ["none", "sm", "md", "lg", "xl"],
+    },
+    width: {
+      control: "inline-radio",
+      options: ["full"],
+    },
+    height: {
+      control: "inline-radio",
+      options: ["big", "mid", "sm"],
+    },
+    disabled: {
+      control: "boolean",
+    },
+    onClick: { action: "clicked" },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Basic: Story = {
+  args: {
+    children: "Button",
+  },
+};
+
+export const Main: Story = {
+  args: {
+    ...Basic.args,
+    variant: "main",
+  },
+};
