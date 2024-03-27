bc8031060e2cf7e1ec2808aa9d8598ae5e10fb1d
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-03-27 18:31:06 +0900

    feat: Button index 파일 추가

---
+++ src/constants/buttonIndex.ts
@@ -0,0 +1,22 @@
+export const BUTTON_INDEX = [
+  {
+    content: "읽고 싶은",
+    actived: false,
+  },
+  {
+    content: "읽는 중",
+    actived: false,
+  },
+  {
+    content: "읽음",
+    actived: false,
+  },
+  {
+    content: "잠시 멈춘",
+    actived: false,
+  },
+  {
+    content: "중단",
+    actived: false,
+  },
+];
