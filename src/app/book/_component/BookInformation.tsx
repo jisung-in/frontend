0d4d92cf7e92f05ac5fa31cab88766364ee1d757
SeungHun6450 <toffg6450@naver.com>
2024-04-12 00:39:16 +0900

    feat: 책 상세보기 별점 부여 기능 추가

--- src/app/book/_component/BookInformation.tsx
+++ src/app/book/_component/BookInformation.tsx
@@ -1,31 +1,65 @@
 import BigStar from "@/assets/img/big-star.svg";
+import EmptyStar from "@/assets/img/empty-star.svg";
+import HalfStar from "@/assets/img/half-star.svg";
 import Pasue from "@/assets/img/pause.svg";
 import ReadStop from "@/assets/img/read-stop.svg";
 import Read from "@/assets/img/read.svg";
 import Reading from "@/assets/img/reading.svg";
 import WantToRead from "@/assets/img/want-to-read.svg";
+import { useState } from "react";
 
 const BookInformation = () => {
+  const [starRate, setStarRate] = useState<number>(0);
+
+  const handleMouseMove = (
+    e: React.MouseEvent<HTMLDivElement>,
+    index: number,
+  ) => {
+    const half = e.nativeEvent.offsetX + index < 27.5;
+    setStarRate(index + (half ? 0.5 : 1));
+  };
+
+  const handleMouseLeave = () => {
+    setStarRate(0);
+  };
+
+  const calculateStarImage = (index: number) => {
+    if (starRate >= index + 1) {
+      return <BigStar width={55} height={55} />;
+    } else if (starRate >= index + 0.5) {
+      return <HalfStar width={55} height={55} />;
+    } else {
+      return <EmptyStar width={55} height={55} />;
+    }
+  };
   return (
     <div className="flex flex-row mt-[22px] mb-[96px] w-[1680px]">
       <div className="min-w-[363px] min-h-[469px] mr-[37px] bg-[#D9D9D9]" />
 
       <div className="flex flex-col flex-grow justify-start font-Pretendard font-medium">
         <div className="flex flex-row items-center mt-[30px] gap-x-[220px]">
-          <div className="flex flex-row">
-            {new Array(5).fill(1).map(() => (
-              <BigStar width={55} hight={55} />
-            ))}
+          <div className="flex flex-col">
+            <div className="flex flex-row">
+              {new Array(5).fill(1).map((_, index: number) => (
+                <div
+                  key={index}
+                  className="relative"
+                  onMouseLeave={handleMouseLeave}
+                  onMouseMove={(e) => handleMouseMove(e, index)}
+                >
+                  {calculateStarImage(index)}
+                </div>
+              ))}
+            </div>
+            <div className="text-[16px] text-[#B1B1B1] mt-[6px] mb-[27px]">
+              평가해요
+            </div>
           </div>
-          <div className="font-intel text-[44px]">4.2</div>
-        </div>
-
-        <div className="flex flex-row gap-x-[445px]">
-          <div className="text-[16px] text-[#B1B1B1] mt-[6px] mb-[27px]">
-            평가해요
-          </div>
-          <div className="text-[16px] text-[#B1B1B1] mt-[6px] mb-[27px]">
-            평균별점
+          <div className="flex flex-col items-center">
+            <div className="font-intel text-[44px]">4.2</div>
+            <div className="text-[16px] text-[#B1B1B1] mt-[6px] mb-[27px]">
+              평균별점
+            </div>
           </div>
         </div>
 
@@ -39,7 +73,7 @@ const BookInformation = () => {
 
         <div className="text-[20px] text-[#656565] w-[680px]">
           책 줄거리 책 줄거리 책 줄거리 책 줄거리 책 줄거리 책 줄거리 책 줄거리
-          책 줄거리 책 줄거리 책 줄거리 책 줄거리 책 줄거리 책 줄거리 책 줄거리
+          책 줄거리 책 줄거리 책 줄거리
         </div>
       </div>
 
