23e5ae4947cfbf7473c5edc400d434dedbde118e
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-04-13 13:12:50 +0900

    refactor: MyBookCard 이미지 렌더링 방식 변경

--- src/app/components/Card/MyInfoCard/MyBookCard.tsx
+++ src/app/components/Card/MyInfoCard/MyBookCard.tsx
@@ -1,13 +1,22 @@
+import Image from "next/image";
 import { BookMain } from "../../Book/Book";
 
-type ImageProps = { children?: React.ReactNode };
-const MyBookCard = ({ children }: ImageProps) => {
+type ImageProps = {
+  postId: number;
+  title: string;
+  image: string;
+  starRate: string;
+};
+
+const MyBookCard = ({ title, image, starRate }: ImageProps) => {
   return (
-    <div className="w-[inherit]">
+    <div>
       <BookMain>
-        <BookMain.BookCover>{children}</BookMain.BookCover>
-        <BookMain.BookTitle>책 제목</BookMain.BookTitle>
-        <BookMain.StarRating>별점 ★★★★★ 4.5</BookMain.StarRating>
+        <BookMain.BookCover className="h-[300px]">
+          <img src={image} className="w-full h-[300px]" alt="bookImage" />
+        </BookMain.BookCover>
+        <BookMain.BookTitle>{title}</BookMain.BookTitle>
+        <BookMain.StarRating>별점 {starRate}</BookMain.StarRating>
       </BookMain>
     </div>
   );
