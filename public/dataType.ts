e3179062129a8cf10b470bcef4e69ee5b5b84768
SeungHun6450 <toffg6450@naver.com>
2024-04-14 00:58:46 +0900

    feat: 메인 페이지 데이터 임시로 public폴더에 data.json으로 관리

---
+++ public/dataType.ts
@@ -0,0 +1,19 @@
+export interface Book {
+  id: number;
+  rank: number;
+  image: string;
+  title: string;
+  publisher: string;
+  author: string;
+  year: number;
+}
+
+export interface Recommend {
+  id: number;
+  image: string;
+  title: string;
+  author: string;
+  talkTitle: string;
+  userName: string;
+  comment: string;
+}
