8c91c7acaa6c053b9ffd4e4bfc6a5b839a43e6b0
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-04-22 17:34:03 +0900

    feat: kakao 도서 검색 api 추가

---
+++ src/hook/reactQuery/search/useGetKakaoResults.ts
@@ -0,0 +1,39 @@
+import axiosInstance from "@/app/api/requestApi";
+import { useQuery } from "@tanstack/react-query";
+
+type SearchingRequest = {
+  target: string;
+};
+
+type SearchingResponse = {
+  documents: BookInfo[];
+};
+
+type BookInfo = {
+  author: string[];
+  contents: string;
+  datetime: string;
+  isbn: string;
+  price: number;
+  publisher: string;
+  sale_price: number;
+  status: string;
+  thumbnail: string;
+  title: string;
+};
+
+export const useGetKakaoResults = ({ target }: SearchingRequest) => {
+  return useQuery<SearchingResponse>({
+    queryKey: ["kakao", target],
+    queryFn: () =>
+      axiosInstance.get(
+        `https://dapi.kakao.com/v3/search/book?target=title&query=${target}&page=1`,
+        {
+          headers: {
+            Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API}`,
+          },
+        },
+      ),
+    throwOnError: true,
+  });
+};
