81b238596a653e41e4d2e7e27951cbe945f1d304
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-04-24 21:23:08 +0900

    refactor: Back Button 뒤로가기 설정 변경

--- src/app/summary/_component/BackButton.tsx
+++ src/app/summary/_component/BackButton.tsx
@@ -7,7 +7,7 @@ const BackButton = () => {
   const router = useRouter();
 
   return (
-    <button onClick={() => router.push("/")}>
+    <button onClick={() => router.back()}>
       <BackArrow />
     </button>
   );
