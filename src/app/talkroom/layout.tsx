bbeac5c97aa70e493d1c5d08563e6638518c2fd5
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-04-24 21:23:57 +0900

    style: layout 가운데 정렬 추가

--- src/app/talkroom/layout.tsx
+++ src/app/talkroom/layout.tsx
@@ -5,7 +5,9 @@ type Props = { children: ReactNode };
 export default function TalkRoomLayout({ children }: Props) {
   return (
     <div className="flex justify-center items-center w-full h-full">
-      <div className="flex flex-col w-[1680px] h-auto">{children}</div>
+      <div className="flex flex-col w-[1680px] h-auto items-center">
+        {children}
+      </div>
     </div>
   );
 }
