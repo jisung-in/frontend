aad2e5f4fca615210278be0a1aae0bfa09724e06
SeungHun6450 <toffg6450@naver.com>
2024-05-07 19:44:08 +0900

    feat: 연관된 토크방 더보기 링크 삭제, 추후 페이지 추가 예정

--- src/app/book/[isbn]/page.tsx
+++ src/app/book/[isbn]/page.tsx
@@ -91,9 +91,7 @@ const page = ({ params }: { params: { isbn: string } }) => {
           <div className="font-SpoqaHanSansNeo font-bold text-[30px] flex flex-row flex-grow">
             <div>연관된 토크방 보기</div>
           </div>
-          <Link
-            href={`/talkroom/${bookDetailData?.title}?page=1&size=10&order=recommend&search=${bookDetailData?.title}&sortbydate=`}
-          >
+          <Link href={`#`}>
             <div className="text-[20px] text-[#74747B] font-Pretendard font-refular flex items-center">
               더보기 {">"}
             </div>
