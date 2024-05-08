b35fad01e23880ad44063cf99b5943f015eee8a1
SeungHun6450 <toffg6450@naver.com>
2024-05-08 21:49:57 +0900

    design: ui에 맞춰 폰트, 글자 크기 수

--- src/app/book/[isbn]/page.tsx
+++ src/app/book/[isbn]/page.tsx
@@ -46,32 +46,32 @@ const page = ({ params }: { params: { isbn: string } }) => {
       </div>
 
       <div className="bg-white">
-        <div className="font-SpoqaHanSansNeo font-bold text-[30px] max-w-[1680px] mx-[120px]">
-          <div className="pt-[43px] mb-[28px]">한줄평을 작성해보세요</div>
-          <div className="relative">
+        <div className="max-w-[1680px] mx-[120px]">
+          <div className="font-SpoqaHanSansNeo font-bold text-[30px] pt-[43px] mb-[28px]">
+            한줄평을 작성해보세요
+          </div>
+          <div className="relative font-Pretendard ">
             <Textarea
               variant="main"
               value={name}
-              className="font-Pretendard font-regular max-w-[1680px] h-[179px]"
+              className="font-regular text-[22px] max-w-[1680px] min-h-[179px]"
               onChange={onNameChange}
               placeholder="한줄평을 자유롭게 작성해보세요."
             />
             <div className="absolute bottom-[26px] right-[36px]">
-              <Button className="font-Pretendard font-medium text-[21px]">
+              <Button className="font-medium text-[21px]">
                 <div className="px-[25px] my-[8px]">등록하기</div>
               </Button>
             </div>
           </div>
 
-          <div className="flex flex-row mt-[63px] mb-[28px]">
-            <div className="flex flex-row gap-x-[19px] flex-grow">
-              <div>유저들의 평가</div>
-              <div className="font-Poppins font-medium text-[#74747B]">
-                3000+
-              </div>
+          <div className="flex flex-row mt-[63px] mb-[28px]  items-center">
+            <div className="flex flex-row gap-x-[19px] flex-grow text-[30px] font-SpoqaHanSansNeo items-center">
+              <div className="font-bold">유저들의 평가</div>
+              <div className="font-medium text-[#74747B]">3000+</div>
             </div>
             <Link href={"/evaluation"}>
-              <div className="text-[20px] text-[#74747B] font-Pretendard font-refular flex items-center">
+              <div className="text-[20px] text-[#74747B] font-Pretendard font-regular">
                 더보기 {">"}
               </div>
             </Link>
@@ -87,12 +87,12 @@ const page = ({ params }: { params: { isbn: string } }) => {
       </div>
 
       <div className="mx-[120px] max-w-[1680px]">
-        <div className="flex flex-row mt-[57px] mb-[27px]">
+        <div className="flex flex-row mt-[57px] mb-[27px] items-center">
           <div className="font-SpoqaHanSansNeo font-bold text-[30px] flex flex-row flex-grow">
-            <div>연관된 토크방 보기</div>
+            연관된 토크방 보기
           </div>
           <Link href={`#`}>
-            <div className="text-[20px] text-[#74747B] font-Pretendard font-refular flex items-center">
+            <div className="text-[20px] text-[#74747B] font-Pretendard font-regular flex items-center">
               더보기 {">"}
             </div>
           </Link>
