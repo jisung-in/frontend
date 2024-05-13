30ad9921442f576b5d2042a44825908d60272ff7
SeungHun6450 <toffg6450@naver.com>
2024-05-13 18:57:01 +0900

    design: HaveNotData 컴포넌트 margin-y값 추가

--- src/app/components/HaveNotData/HaveNotData.tsx
+++ src/app/components/HaveNotData/HaveNotData.tsx
@@ -7,7 +7,7 @@ interface HaveNotDataProps {
 
 const HaveNotData = ({ content }: HaveNotDataProps) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center my-[140px]">
       <Image src={NoData} alt={"Have Not Data"} width={150} height={141} />
       <div className="font-SpoqaHanSansNeo font-bold text-[30px] text-[#624E45]">
         {content} 없습니다
