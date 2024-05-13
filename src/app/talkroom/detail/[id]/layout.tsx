96a6342ba464b3de4c028ecb5b73c5a78126fa38
SeungHun6450 <toffg6450@naver.com>
2024-05-13 21:10:06 +0900

    design: 토크방 상세 보기 디자인 변경

--- src/app/talkroom/detail/[id]/layout.tsx
+++ src/app/talkroom/detail/[id]/layout.tsx
@@ -5,7 +5,7 @@ type Props = { children: ReactNode };
 export default function TalkRoomLayout({ children }: Props) {
   return (
     <div className="flex justify-center items-center w-full h-full bg-[#FBF7F0]">
-      <div className="flex flex-col w-[1402px] h-auto">{children}</div>
+      <div className="flex flex-col w-[1062px] h-auto">{children}</div>
     </div>
   );
 }
--- src/app/talkroom/detail/[id]/page.tsx
+++ src/app/talkroom/detail/[id]/page.tsx
@@ -21,6 +21,35 @@ const Page = ({ params }: { params: { id: number } }) => {
 
       <hr className="border-[6px] border-[#F5EFE5] mt-[47px] mb-[42px]" />
 
+      <div className="flex flex-col items-center mt-[37px] mb-[140px]">
+        <div className="font-SpoqaHanSansNeo font-bold text-[#80685D] text-[30px] mb-[43px]">
+          참가 조건에 부합하여 의견 작성이 가능합니다
+        </div>
+        <Button
+          width="register"
+          height="xl2"
+          className="font-Pretendard font-semibold text-[28px]"
+        >
+          <Link className="w-full" href={"/talkroom/comment"}>
+            등록하기
+          </Link>
+        </Button>
+      </div>
+
+      <div className="flex flex-col items-center mt-[37px] mb-[140px]">
+        <div className="font-SpoqaHanSansNeo font-bold text-[#656565] text-[30px] mb-[43px]">
+          참가 조건에 부합하지 않습니다
+        </div>
+        <Button
+          width="register"
+          height="xl2"
+          variant={"notCondition"}
+          className="font-Pretendard font-semibold text-[28px] text-[#828282] pointer-events-none"
+        >
+          등록하기
+        </Button>
+      </div>
+
       <div className="font-Pretendard font-semibold text-[21px] text-[#818181] mb-[28px]">
         의견 999+
       </div>
@@ -36,33 +65,6 @@ const Page = ({ params }: { params: { id: number } }) => {
         />
       ))}
       <MySpeechBubble content={"나의 의견 내용 들어갈 곳 입니다."} />
-
-      <div className="flex flex-col items-center mt-[37px] mb-[140px]">
-        <div className="font-SpoqaHanSansNeo font-bold text-[#80685D] text-[30px] mb-[43px]">
-          참가 조건에 부합하여 의견 작성이 가능합니다
-        </div>
-        <Button
-          height="xl2"
-          className="font-Pretendard font-semibold text-[28px]"
-        >
-          <Link className="w-full" href={"/talkroom/comment"}>
-            등록하기
-          </Link>
-        </Button>
-      </div>
-
-      <div className="flex flex-col items-center mt-[37px] mb-[140px]">
-        <div className="font-SpoqaHanSansNeo font-bold text-[#656565] text-[30px] mb-[43px]">
-          참가 조건에 부합하지 않습니다
-        </div>
-        <Button
-          height="xl2"
-          variant={"notCondition"}
-          className="font-Pretendard font-semibold text-[28px] text-[#828282] pointer-events-none"
-        >
-          등록하기
-        </Button>
-      </div>
     </div>
   );
 };
