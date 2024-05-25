82ad7baca6a38a9c9b4a0106939554b70a912f46
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-05-25 23:48:24 +0900

    feat: 페이지 별 권한 관리하는 middleware 생성

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
-import { NextResponse } from "next/server";
-export async function middleware() {
-  // const session = await auth();
-  // if (!session) {
-  //   return NextResponse.redirect("/login");
-  // }
+import { NextRequest, NextResponse } from "next/server";
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("accessToken");
+
+  return token
+    ? NextResponse.next()
+    : NextResponse.redirect(new URL("/", request.url));
 }
 
 export const config = {
