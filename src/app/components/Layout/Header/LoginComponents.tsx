20b6159bf3fb030a2dd1f40e158c68a64058546a
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-05-25 23:48:06 +0900

    refactor: 로그인 관련 로직 추가에 따른 layout header 변경

--- src/app/components/Layout/Header/LoginComponents.tsx
+++ src/app/components/Layout/Header/LoginComponents.tsx
@@ -2,23 +2,25 @@
 import { Button } from "../../Button/Button";
 import React from "react";
 import { useRouter } from "next/navigation";
+import { useLogin } from "@/hook/useLogin";
 
 export const LoginComponent = () => {
   const router = useRouter();
+  const { isLoggedIn } = useLogin();
   return (
-    <div className="w-[100px]">
-      {/* {me?.user ? (
+    <div className="w-[100px] sm:w-[50px]">
+      {isLoggedIn ? (
         <img
           alt="프로필"
-          src={me?.user?.image!}
-          className="rounded-[50%] w-[50px] h-[50px]"
+          src={localStorage.getItem("userImage") ?? ""}
+          className="rounded-[50%] w-[50px] h-[50px] cursor-pointer"
           onClick={() => router.push("/my")}
         />
-      ) : ( */}
-      <Button height="md" onClick={() => router.push("/login")}>
-        로그인
-      </Button>
-      {/* )} */}
+      ) : (
+        <Button height="md" onClick={() => router.push("/login")}>
+          로그인
+        </Button>
+      )}
     </div>
   );
 };
