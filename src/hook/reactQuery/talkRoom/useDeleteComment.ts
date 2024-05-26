57eb63c50b343c7af4d4cf203f28ea34e60953d0
SeungHun6450 <toffg6450@naver.com>
2024-05-26 12:31:17 +0900

    feat: 토크방 의견  삭제 hook 생성

---
+++ src/hook/reactQuery/talkRoom/useDeleteComment.ts
@@ -0,0 +1,15 @@
+import axiosInstance from "@/app/api/requestApi";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+export const useDeleteComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (commentId: number) =>
+      axiosInstance.delete(`/v1/comments/${commentId}`),
+    onSuccess: (_, commentId) => {
+      queryClient.invalidateQueries({
+        queryKey: ["talkroom", "like", commentId],
+      });
+    },
+  });
+};
