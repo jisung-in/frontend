80f54ced8e393bfbdaef621ff8c6096b311f45fc
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-05-27 20:54:55 +0900

    refactor: 쿼리에 key(필터) 추가

--- src/hook/reactQuery/my/useGetMyTalkRooms.ts
+++ src/hook/reactQuery/my/useGetMyTalkRooms.ts
@@ -2,8 +2,8 @@ import axiosInstance from "@/app/api/requestApi";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetMyTalkRooms = (key: string) => {
-  return useQuery({
-    queryKey: ["my", "talkRooms"],
+  return useQuery<any>({
+    queryKey: ["my", "talkRooms", key],
     queryFn: async () =>
       await axiosInstance.get(
         `${process.env.NEXT_PUBLIC_SERVER}/v1/users/talk-rooms?page=1&size=10&commentFilter=${key === "replied"}&likeFilter=${key === "liked"}`,
