3d32c6cb78ef9144b34248393b7457404818a1b5
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-05-31 01:16:00 +0900

    refactor: 디자이너의 요구사항 변경으로 인한 svg 파일 변경

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,18 +1,11 @@
 import SummaryCard from "./_component/SummaryCard";
-import Profile from "@/assets/img/profile.png";
-import Star from "@/assets/img/start-rate.svg";
+import Star from "@/assets/img/star-rate.svg";
 import Answer from "@/assets/img/answer-room.svg";
 import Review from "@/assets/img/book-review.svg";
 import Create from "@/assets/img/create-room.svg";
-import Wheel from "@/assets/img/wheel.svg";
-import Image from "next/image";
-import MyButton from "./_component/MyButton";
-
-const userData = {
-  id: "이름 (아이디)",
-  profile: "",
-  decribe: "한줄 소개를 작성해 보세요. 한줄 소개를 작성해 주실래요?",
-};
+import Profile from "./_component/Profile";
+import MyProfile from "@/assets/img/MyProfile.svg";
+import BackButton from "../summary/_component/BackButton";
 
 const data = [
   {
@@ -21,17 +14,17 @@ const data = [
     path: "/summary/star",
   },
   {
-    title: "서평",
+    title: "한줄평",
     icon: <Answer />,
     path: "/summary/review",
   },
   {
-    title: "생성방",
+    title: "토크방",
     icon: <Create />,
     path: "/summary/room",
   },
   {
-    title: "답변방",
+    title: "독서상태",
     icon: <Review />,
     path: "/summary/state",
   },
@@ -40,31 +33,14 @@ const data = [
 const MyPage = () => {
   return (
     <>
-      <div className="flex flex-col px-[5%] py-[5%] gap-5 relative">
-        <Image
-          src={Profile}
-          className="rounded-[50%]"
-          alt="프로필"
-          width={180}
-          height={180}
-          priority
-        />
-        <div className="absolute right-[10px]">
-          <MyButton width={100}>로그아웃</MyButton>
+      <span className="flex gap-[10px] px-[5%] py-[5%] flex-col w-full text-[30px] sm:text-[18px] font-bold">
+        <BackButton />
+        <div className="flex items-center gap-[10px]">
+          마이페이지
+          <MyProfile />
         </div>
-
-        <div className="flex align-center gap-[5%] sm:flex-col sm:gap-[20px]">
-          <span className="text-[27px] font-[700]">{userData.id}</span>
-
-          <MyButton width={133} path="/my/edit">
-            프로필 수정
-            <Wheel />
-          </MyButton>
-        </div>
-        <span className="text-[16px] text-gray-50 font-[500]">
-          {userData.decribe}
-        </span>
-      </div>
+      </span>
+      <Profile />
       <div className="block w-[100%] bg-gray-30 border-t-2">
         <br />
       </div>
