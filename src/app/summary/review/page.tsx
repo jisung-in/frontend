a3eed34894c53043a7ea03fdce7fadae5d71d50f
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-05-31 01:12:17 +0900

    refactor: My 페이지 부문 DropDown관련 로직 변경

--- src/app/summary/review/page.tsx
+++ src/app/summary/review/page.tsx
@@ -1,17 +1,38 @@
 "use client";
 
 import MyEvaluationCard from "@/app/components/Card/EvaluationCard/MyEvaluationCard";
+import Dropdown from "@/app/components/DropDown/DropDown";
 import { useGetMyReview } from "@/hook/reactQuery/my/useGetMyReview";
+import { useState } from "react";
+
+const filterList: { [key: string]: string | "" } = {
+  "나의 별점 높은 순": "rating_asc",
+  "나의 별점 낮은 순": "rating_desc",
+  "평균 별점 높은 순": "rating_avg_asc",
+  "평균 별점 낮은 순": "rating_avg_desc",
+};
 
 const ReviewPage = () => {
-  const { data: evaluationUser } = useGetMyReview();
+  const [starFilter, setStartFilter] = useState("나의 별점 높은 순");
+  const { data: evaluationUser } = useGetMyReview({
+    order: filterList[starFilter],
+  });
+
+  console.log(evaluationUser?.reviewContents);
 
   return (
-    <div className="flex justify-center ">
-      <div className="grid grid-cols-2 gap-[20px] w-[100%]">
-        {/* {evaluationUser?.map((data: any) => (
-          <MyEvaluationCard key={data.id} data={data} />
-        ))} */}
+    <div className="flex flex-col w-full h-full items-center">
+      <div className="flex w-full justify-end pr-[10%] pb-[10px]">
+        <Dropdown
+          items={Object.keys(filterList)}
+          selectedItem={starFilter}
+          setSelectedItem={setStartFilter}
+        />
+      </div>
+      <div className="grid grid-cols-2 gap-[20px] w-[90%]">
+        {evaluationUser?.reviewContents.queryResponse?.map((data: any) => (
+          <MyEvaluationCard key={data.reviewId} data={data} />
+        ))}
       </div>
     </div>
   );
