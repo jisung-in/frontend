e5afd2f361808428e9ca925db8500367dd09517f
SeungHun6450 <toffg6450@naver.com>
2024-06-21 00:24:16 +0900

    refactor, design: pivoting한 ui에 따른 토크룸카드와 main페이지 적용

--- src/app/components/Card/MainPageCard/TalkRoomCard.tsx
+++ src/app/components/Card/MainPageCard/TalkRoomCard.tsx
@@ -1,11 +1,12 @@
 import BookTitle from "@/assets/img/book-title.svg";
 import Like from "@/assets/img/like.svg";
 import NoImage from "@/assets/img/no-image.png";
-import NotLike from "@/assets/img/not-like-white.svg";
+import NotLike from "@/assets/img/not-like.svg";
 import Profile from "@/assets/img/profile.png";
-import ThemeTitle from "@/assets/img/theme-title.svg";
 import { useCreateRoomLike } from "@/hook/reactQuery/talkRoom/useCreateRoomLike";
 import { useDeleteRoomLike } from "@/hook/reactQuery/talkRoom/useDeleteRoomLike";
+import { useGetComments } from "@/hook/reactQuery/talkRoom/useGetComments";
+import timeLapse from "@/util/timeLapse";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -24,7 +25,7 @@ type TalkRoomCardProps = {
     bookThumbnail: string;
     likeCount: number;
     readingStatuses?: string[];
-    registeredDateTime?: string;
+    registeredDateTime: string;
     creatorId: number;
   };
   userId: number;
@@ -38,6 +39,7 @@ const TalkRoomCard: React.FC<TalkRoomCardProps> = ({
   isBest,
   isLike: initialIsLike,
 }) => {
+  const { data: commentsData } = useGetComments(data.id);
   const [count, setCount] = useState<number>(data.likeCount);
   const [isLike, setIsLike] = useState<boolean>(initialIsLike);
   const addTalkRoomLike = useCreateRoomLike();
@@ -72,288 +74,114 @@ const TalkRoomCard: React.FC<TalkRoomCardProps> = ({
   };
 
   return (
-    <div
-      className="relative 
-        sm:w-[288px] 
-        md:w-[317px] 
-        lg:w-[346px] 
-        xl:w-[375px] 
-        xl2:w-[405px] 
-        sm:h-[235px]
-        md:h-[258px]
-        lg:h-[282px]
-        xl:h-[306px]
-        xl2:h-[330px]
-        sm:rounded-[10px]
-        md:rounded-[12px]
-        lg:rounded-[14px]
-        xl:rounded-[16px]
-        xl2:rounded-[17px]
-        bg-[#fff] border border-[#F4E4CE] font-Pretendard overflow-hidden"
-    >
+    <div className="min-w-[328px] w-[84vw] max-w-[1280px] max-h-[236px] rounded-[17px] bg-[#fff] border border-[#F4E4CE] font-Pretendard overflow-hidden">
       <Link href={`/talkroom/detail/${data.id}`}>
-        <div
-          className="
-        sm:top-[-10%]
-        md:top-[-15%]
-        lg:top-[-20%]
-        xl:top-[-25%]
-        xl2:top-[-30%]
-        sm:h-[100px]
-        md:h-[120px]
-        lg:h-[147px]
-        xl:h-[174px]
-        xl2:h-[200px]
-        absolute inset-0 transform -skew-y-[10deg] bg-[#80685D] "
-        ></div>
-        <div className="absolute inset-0 flex justify-center items-center">
-          <div
-            className="flex flex-col 
-            sm:m-[15px]
-            md:m-[17px]
-            lg:m-[20px]
-            xl:m-[23px]
-            xl2:m-[26px]
-            w-full"
-          >
-            <div
-              className="flex flex-row
-              sm:mb-[8px]
-              md:mb-[8px]
-              lg:mb-[8px]
-              xl:mb-[8px]
-              xl2:mb-[8px]"
-            >
-              <div className="flex flex-grow">
-                <div
-                  className="relative 
-                  sm:min-w-[70px]
-                  sm:max-w-[70px]
-                  md:min-w-[77px]
-                  md:max-w-[77px]
-                  lg:min-w-[85px]
-                  lg:max-w-[85px]
-                  xl:min-w-[95px]
-                  xl:max-w-[95px]
-                  xl2:min-w-[100px]
-                  xl2:max-w-[100px]
-                  sm:min-h-[100px] 
-                  sm:max-h-[100px] 
-                  md:min-h-[110px] 
-                  md:max-h-[110px] 
-                  lg:min-h-[120px] 
-                  lg:max-h-[120px] 
-                  xl:min-h-[130px] 
-                  xl:max-h-[130px] 
-                  xl2:min-h-[140px]
-                  xl2:max-h-[140px]
-                  "
-                >
-                  {data.bookThumbnail ? (
-                    <Image
-                      className="border border-[#F4E4CE]"
-                      src={data.bookThumbnail}
-                      alt="책 표지"
-                      fill
-                    />
-                  ) : (
-                    <Image
-                      className="border border-[#F4E4CE]"
-                      src={data.bookThumbnail ? data.bookThumbnail : NoImage}
-                      alt="책 표지"
-                      fill
-                    />
-                  )}
-                </div>
-                <div
-                  className="
-                  sm:ml-2                   
-                  md:ml-2.5                   
-                  lg:ml-3.5                   
-                  xl:ml-4                   
-                  xl2:ml-4                   
-                  sm:mt-2
-                  md:mt-2.5 
-                  lg:mt-3.5 
-                  xl:mt-4 
-                  xl2:mt-4 "
-                >
-                  <BookTitle />
-                </div>
-                <div
-                  className="
-                  sm:mt-2
-                  md:mt-2 
-                  lg:mt-3 
-                  xl:mt-3 
-                  xl2:mt-4 
-                  sm:ml-[6px]
-                  md:ml-[7px]
-                  lg:ml-[8px]
-                  xl:ml-[9px]
-                  xl2:ml-[9px]
-                  flex-grow text-[#656565] text-white"
-                >
-                  <div
-                    className="
-                    sm:text-[13px]
-                    md:text-sm
-                    lg:text-[15px]
-                    xl:text-base
-                    xl2:text-[17px]
-                    font-semibold mb-1 overflow-hidden line-clamp-1"
-                  >
-                    {data.bookName}
-                  </div>
-                  <div
-                    className="
-                    sm:text-[10px]
-                    md:text-[11px] 
-                    lg:text-xs
-                    xl:text-[13px] 
-                    xl2:text-sm 
-                    overflow-hidden line-clamp-1"
-                  >
-                    {data.bookAuthor}
-                  </div>
-                </div>
-              </div>
-
-              <div className="flex flex-col items-center">
-                <IconButton onClick={changeIsLike}>
-                  {isLike ? (
-                    <div>
-                      <Like width={21} height={19} />
-                      <div
-                        className="text-[13px] 
-                        font-Inter font-regular text-[#F24D4D]"
-                      >
-                        {count}
-                      </div>
-                    </div>
-                  ) : (
-                    <>
-                      <NotLike width={21} height={19} />
-                      <div
-                        className="text-[13px] 
-                        font-Inter font-regular text-white"
-                      >
-                        {count}
-                      </div>
-                    </>
-                  )}
-                </IconButton>
-              </div>
-            </div>
-
-            <div
-              className="
-              sm:mb-0.5
-              md:mb-0.5
-              lg:mb-1.5
-              xl:mb-2
-              xl2:mb-3.5
-              flex flex-row font-semibold items-center gap-x-[7px] "
-            >
-              <ThemeTitle />
-              <div
-                className="
-                sm:text-[15px]
-                md:text-base
-                lg:text-[17px]
-                xl:text-lg
-                xl2:text-xl
-                flex flex-row overflow-hidden line-clamp-1"
-              >
-                {data.title}
-              </div>
-              {isBest && (
-                <div
-                  className="
-                  sm:text-[10px]
-                  md:text-[11px]
-                  lg:text-xs
-                  xl:text-[13px]
-                  xl2:text-sm
-                  flex items-center bg-transparent leading-tight text-[#F24D4D] border-2 border-[#F24D4D] rounded-[4px] px-[6px]"
-                >
-                  BEST
-                </div>
+        <div className="flex justify-center mx-[32px] my-[35px]">
+          <div className="flex grow">
+            <div className="relative min-w-[110px] max-w-[110px] min-h-[150px] max-h-[150px]">
+              {data.bookThumbnail ? (
+                <Image
+                  className="border border-[#F4E4CE]"
+                  src={data.bookThumbnail}
+                  alt="책 표지"
+                  fill
+                />
+              ) : (
+                <Image
+                  className="border border-[#F4E4CE]"
+                  src={data.bookThumbnail ? data.bookThumbnail : NoImage}
+                  alt="책 표지"
+                  fill
+                />
               )}
             </div>
 
-            <div
-              className="
-              sm:mb-1.5
-              md:mb-2.5 
-              lg:mb-3
-              xl:mb-3.5 
-              xl2:mb-4 
-              sm:h-10
-              md:h-10
-              lg:h-11
-              xl:h-12
-              xl2:h-12
-              sm:text-[13px]
-              md:text-sm
-              lg:text-[15px]
-              xl:text-base
-              xl2:text-base
-              font-medium text-[#656565] overflow-hidden line-clamp-2"
-            >
-              {data.content}
-            </div>
+            <div className="flex flex-col ml-[25px] text-black w-[68vw] max-w-[1044px]">
+              <div className="flex flex-row items-center">
+                <div className="flex flex-row grow font-semibold items-center gap-x-[8px]">
+                  {/* 기록, 질문, 평가 테마에 맞게 데이터 추가되면 작성 */}
+                  <div className="flex flex-row overflow-hidden line-clamp-1 text-[22px]">
+                    {data.title}
+                  </div>
+                  {isBest && (
+                    <div className="text-sm flex items-center bg-transparent leading-tight text-[#F24D4D] border-2 border-[#F24D4D] rounded-[4px] px-[6px]">
+                      BEST
+                    </div>
+                  )}
+                </div>
 
-            <hr
-              className="
-              sm:mb-2.5
-              md:mb-3
-              lg:mb-3
-              xl:mb-3
-              xl2:mb-3
-              w-full border border-[#F4E4CE]"
-            />
-
-            <div className="flex flex-row gap-x-[9px] items-center">
-              <div>
-                <Image
-                  className="
-                  sm:w-[18px] 
-                  md:w-[20px] 
-                  lg:w-[22px] 
-                  xl:w-[24px] 
-                  xl2:w-[26px] 
-                  sm:h-[18px] 
-                  md:h-[20px] 
-                  lg:h-[22px] 
-                  xl:h-[24px] 
-                  xl2:h-[26px]
-                  sm:border rounded-[12px]
-                  md:border rounded-[13px]
-                  lg:border rounded-[14px]
-                  xl:border rounded-[15px]
-                  xl2:border rounded-[16px]"
-                  src={
-                    data.profileImage !== "image" ? data.profileImage : Profile
-                  }
-                  alt="프로필"
-                  width={26}
-                  height={26}
-                  priority
-                />
+                <div className="flex text-[17px] text-[#7E7E7E]">
+                  {timeLapse(data.registeredDateTime)}
+                </div>
               </div>
-              <div
-                className="
-               sm:text-[12px]
-               md:text-[13px]
-               lg:text-[14px]
-               xl:text-[15px]
-               xl2:text-[15px]
-              font-medium"
-              >
-                {data.username}
+
+              <div className="flex flex-row items-center gap-x-[5px]">
+                <BookTitle />
+                <div className="text-[19px] font-medium mb-1 overflow-hidden line-clamp-1">
+                  {data.bookName}
+                </div>
+              </div>
+
+              <div className="flex flex-row gap-x-2.5 items-center">
+                <div>
+                  <Image
+                    className="w-[18px] h-[18px] rounded-[50%]"
+                    src={
+                      data.profileImage !== "image"
+                        ? data.profileImage
+                        : Profile
+                    }
+                    alt="프로필"
+                    width={18}
+                    height={18}
+                    priority
+                  />
+                </div>
+                <div className="text-base font-medium text-[#777]">
+                  {data.username}
+                </div>
+              </div>
+
+              <div className="flex items-center mt-[10px] bg-[#FBF7F0] max-h-[40px] rounded-[8px] px-[20px] py-[17px]">
+                {commentsData?.queryResponse[0] ? (
+                  <div className="flex items-center gap-x-2">
+                    <div className="font-medium text-[15px] text-[#777]">
+                      {`(${timeLapse(commentsData?.queryResponse[0].registeredDateTime)})`}
+                    </div>
+                    <div className="font-semibold text-lg">
+                      {`"${commentsData?.queryResponse[0].content}"`}
+                    </div>
+                  </div>
+                ) : (
+                  <div className="flex items-center">
+                    아직 작성된 의견이 없습니다.
+                  </div>
+                )}
               </div>
             </div>
+          </div>
+
+          <div className="flex flex-col items-center mt-1.5 ml-3">
+            <IconButton onClick={changeIsLike}>
+              {isLike ? (
+                <div>
+                  <Like width={21} height={19} />
+                  <div
+                    className="text-[13px] 
+                        font-Inter font-regular text-[#F24D4D]"
+                  >
+                    {count}
+                  </div>
+                </div>
+              ) : (
+                <>
+                  <NotLike width={21} height={19} />
+                  <div className="text-[13px] font-Inter font-regular text-[#624E45]">
+                    {count}
+                  </div>
+                </>
+              )}
+            </IconButton>
           </div>
         </div>
       </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 "use client";
-import BestSeller from "@/assets/img/best-seller.svg";
-import ManyTalkRoomBook from "@/assets/img/many-talk-room-book.svg";
-import PopularTalkRoom from "@/assets/img/popular-talk-room.svg";
-import RecentTalkRoom from "@/assets/img/recent-make-talk-room.svg";
 import { useGetBookRank } from "@/hook/reactQuery/book/useGetBookRank";
 import { useGetMyDetail } from "@/hook/reactQuery/my/useGetMyDetail";
-import { useGetRoomBookOrder } from "@/hook/reactQuery/talkRoom/useGetRoomBookOrder";
 import { useGetRoomLike } from "@/hook/reactQuery/talkRoom/useGetRoomLike";
 import { useGetRooms } from "@/hook/reactQuery/talkRoom/useGetRooms";
 import { useLogin } from "@/hook/useLogin";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import ManyTalkRoomBookCard from "./components/Card/MainPageCard/ManyTalkRoomBookCard";
 import TalkRoomCard from "./components/Card/MainPageCard/TalkRoomCard";
 import HaveNotData from "./components/HaveNotData/HaveNotData";
-import ResizeImage from "./components/ResizeImage/ResizeImage";
-import RankSwiper from "./components/Swiper/RankSwiper";
-import TalkRoomCardSwiper from "./components/Swiper/TalkRoomCardSwiper";
-import { ThemeMain } from "./components/Theme/Theme";
 type TalkRoom = {
   id: number;
   profileImage: string;
@@ -41,7 +29,6 @@ type TalkRoomBookOrder = {
   dateTime: string;
 };
 const page = () => {
-  const [isSwiper, setIsSwiper] = useState(false);
   const { isLoggedIn } = useLogin();
   const { data: talkRoomLikeIds } = isLoggedIn
     ? useGetRoomLike()
@@ -50,281 +37,70 @@ const page = () => {
     ? useGetMyDetail()
     : { data: { userId: -1, userImage: "", userName: "" } };
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSwiper(window.innerWidth <= 1800);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const { data: popularData } = useGetRooms({
-    page: 1,
-    size: 4,
-    order: "recommend",
-    search: "",
-    sortbydate: "",
-  });
   const { data: recentData } = useGetRooms({
     page: 1,
-    size: 4,
+    size: 3,
     order: "recent",
     search: "",
     sortbydate: "",
   });
   const { data: bookRankData } = useGetBookRank();
-  const { data: talkRoomManyBookData } = useGetRoomBookOrder({
-    page: 1,
-    size: 12,
-    order: "comment",
-  });
-
   return (
-    <div className="bg-[#FFF] w-full">
+    <div className="flex flex-col items-center max-w-[1280px]">
       <div
         className="
-        sm:mt-[25px]
-        md:mt-[34px]
-        lg:mt-[40px]
-        xl:mt-[48px]
-        xl2:mt-[56px] 
-        sm:px-[16px]
-        md:px-[42px]
-        lg:px-[68px]
-        xl:px-[89px]
-        xl2:px-[120px]
-        sm:mb-[25px]
-        md:mb-[36px]
-        lg:mb-[48px]
-        xl:mb-[60px]
-        xl2:mb-[72px]"
+        xl2:mt-[85px] 
+        xl2:mb-[73px]"
       >
-        <div
-          className="
-          sm:mb-[17px]
-          md:mb-[19px]
-          lg:mb-[22px]
-          xl:mb-[24px]
-          xl2:mb-[26px]"
-        >
-          <ThemeMain>
-            <ThemeMain.MainTheme>
-              <div
-                className="
-                flex grow items-center 
-                sm:gap-x-1.5 md:gap-x-2 lg:gap-x-2.5 xl:gap-x-2.5 xl2:gap-x-3"
-              >
-                <div>인기있는 토크방</div>
-                <ResizeImage src={PopularTalkRoom} alt="인기있는 토크방" />
-              </div>
-            </ThemeMain.MainTheme>
-            <ThemeMain.Show>
-              <Link href={"/talkroom/?order=recommend&sortbydate=&page=1"}>
-                <div className="w-full">전체보기 {">"}</div>
-              </Link>
-            </ThemeMain.Show>
-          </ThemeMain>
-        </div>
-        {popularData && popularData.queryResponse.length > 0 ? (
-          isSwiper ? (
-            <TalkRoomCardSwiper
-              talkRooms={popularData.queryResponse}
-              userId={myDetailData?.userId || -1}
-              isBest={true}
-              userLikeTalkRoomIds={talkRoomLikeIds?.talkRoomIds || []}
-            />
-          ) : (
-            <div className="flex flex-row xl2:gap-x-[20px]">
-              {popularData.queryResponse.map((data: TalkRoom) => {
-                const isLike =
-                  isLoggedIn &&
-                  (talkRoomLikeIds?.talkRoomIds || []).includes(data.id);
-                return (
-                  <TalkRoomCard
-                    key={data.id}
-                    data={data}
-                    userId={myDetailData?.userId || -1}
-                    isBest={true}
-                    isLike={isLike}
-                  />
-                );
-              })}
-            </div>
-          )
-        ) : (
-          <HaveNotData content={"인기있는 토크방이"} />
-        )}
+        {/* 기록, 질문, 평가 컴포넌트 위치 */}
       </div>
-      <div className="bg-[#FBF7F0] py-[1px]">
-        <div
-          className="
-            sm:my-[26px] 
-            md:my-[34px]
-            lg:my-[42px]
-            xl:my-[48px]
-            xl2:my-[56px]
-            sm:px-[16px]
-            md:px-[42px]
-            lg:px-[68px]
-            xl:px-[89px]
-            xl2:px-[120px]
-            "
-        >
-          <ThemeMain.MainTheme>
-            <div
-              className="flex
-              sm:mb-[15px] md:mb-[17px] lg:mb-[19px] xl:mb-[19px] xl2:mb-[21px]"
-            >
-              <div
-                className="
-                flex grow items-center
-                sm:gap-x-1.5 md:gap-x-2 lg:gap-x-2.5 xl:gap-x-2.5 xl2:gap-x-3 "
-              >
-                <div>베스트 셀러</div>
-                <ResizeImage src={BestSeller} alt="베스트 셀러" />
-              </div>
-            </div>
-          </ThemeMain.MainTheme>
-          {bookRankData && bookRankData.length > 0 ? (
-            <RankSwiper data={bookRankData} />
-          ) : (
-            <HaveNotData content={"베스트 셀러가"} />
-          )}
-        </div>
-      </div>
-      <div
-        className="
-        sm:my-[26px] 
-        md:my-[34px]
-        lg:my-[42px]
-        xl:my-[48px]
-        xl2:my-[56px]
-        sm:px-[16px]
-        md:px-[42px]
-        lg:px-[68px]
-        xl:px-[89px]
-        xl2:px-[120px]"
-      >
-        <div
-          className="
-          sm:mb-[17px]
-          md:mb-[19px]
-          lg:mb-[22px]
-          xl:mb-[24px]
-          xl2:mb-[26px]"
-        >
-          <ThemeMain>
-            <ThemeMain.MainTheme>
-              <div
-                className="flex grow items-center
-                sm:gap-x-1.5 md:gap-x-2 lg:gap-x-2.5 xl:gap-x-2.5 xl2:gap-x-3"
-              >
-                <div>최근 생성된 토크방</div>
-                <ResizeImage src={RecentTalkRoom} alt="최근 생성된 토크방" />
-              </div>
-            </ThemeMain.MainTheme>
-            <ThemeMain.Show>
-              <Link href={"/talkroom/?order=recent&page=1"}>
-                <div className="w-full">전체보기 {">"}</div>
-              </Link>
-            </ThemeMain.Show>
-          </ThemeMain>
-        </div>
+
+      <div className="">
+        <div className="mb-[25px]">{/* 버튼 */}</div>
         {recentData && recentData.queryResponse.length > 0 ? (
-          isSwiper ? (
-            <TalkRoomCardSwiper
-              talkRooms={recentData.queryResponse}
-              userId={myDetailData?.userId || -1}
-              isBest={false}
-              userLikeTalkRoomIds={talkRoomLikeIds?.talkRoomIds || []}
-            />
-          ) : (
-            <div className="flex flex-row xl2:gap-x-[20px]">
-              {recentData.queryResponse.map((data: TalkRoom) => {
-                const isLike =
-                  isLoggedIn &&
-                  (talkRoomLikeIds?.talkRoomIds || []).includes(data.id);
-                return (
-                  <TalkRoomCard
-                    key={data.id}
-                    data={data}
-                    userId={myDetailData?.userId || -1}
-                    isBest={false}
-                    isLike={isLike}
-                  />
-                );
-              })}
-            </div>
-          )
+          <div className="flex flex-col gap-y-[25px]">
+            {recentData.queryResponse.map((data: TalkRoom) => {
+              const isLike =
+                isLoggedIn &&
+                (talkRoomLikeIds?.talkRoomIds || []).includes(data.id);
+              return (
+                <TalkRoomCard
+                  key={data.id}
+                  data={data}
+                  userId={myDetailData?.userId || -1}
+                  isBest={false}
+                  isLike={isLike}
+                />
+              );
+            })}
+          </div>
         ) : (
           <HaveNotData content={"최근 생성된 토크방이"} />
         )}
       </div>
-      <div
-        className="bg-[#FBF7F0] pt-[1px] 
-        sm:pb-[17px]
-        md:pb-[28px]
-        lg:pb-[38px]
-        xl:pb-[48px]
-        xl2:pb-[56px]"
-      >
-        <div
-          className="            
-          sm:mt-[26px] 
-          md:mt-[34px]
-          lg:mt-[42px]
-          xl:mt-[48px]
-          xl2:mt-[56px]
-          sm:px-[16px]
-          md:px-[42px]
-          lg:px-[68px]
-          xl:px-[89px]
-          xl2:px-[120px]"
-        >
-          <ThemeMain>
-            <ThemeMain.MainTheme>
-              <div
-                className="
-                flex items-center
-                sm:gap-x-1.5 md:gap-x-2 lg:gap-x-2.5 xl:gap-x-2.5 xl2:gap-x-3
-                sm:mb-[17px]
-                md:mb-[19px]
-                lg:mb-[22px]
-                xl:mb-[24px]
-                xl2:mb-[26px]"
-              >
-                <div>토크 많은 책</div>
-                <ResizeImage src={ManyTalkRoomBook} alt="토크 많은 책" />
-              </div>
-            </ThemeMain.MainTheme>
-          </ThemeMain>
-          {talkRoomManyBookData && talkRoomManyBookData.length > 0 ? (
+
+      {/* <div className=":my-[56px] px-[120px] ">
+        <ThemeMain.MainTheme>
+          <div
+            className="flex
+              sm:mb-[15px] md:mb-[17px] lg:mb-[19px] xl:mb-[19px] xl2:mb-[21px]"
+          >
             <div
               className="
-              grid gap-y-3
-              sm:grid-cols-3
-              md:grid-cols-3
-              lg:grid-cols-4
-              xl:grid-cols-5
-              xl2:grid-cols-6
-            "
+                flex grow items-center
+                sm:gap-x-1.5 md:gap-x-2 lg:gap-x-2.5 xl:gap-x-2.5 xl2:gap-x-3 "
             >
-              {talkRoomManyBookData.map((data: TalkRoomBookOrder) => (
-                <Link key={data.isbn} href={`/book/${data.isbn}`}>
-                  <ManyTalkRoomBookCard data={data} />
-                </Link>
-              ))}
+              <div>지성인의 베스트 셀러</div>
+              <ResizeImage src={BestSeller} alt="베스트 셀러" />
             </div>
-          ) : (
-            <HaveNotData content={"토크 많은 책이"} />
-          )}
-        </div>
-      </div>
+          </div>
+        </ThemeMain.MainTheme>
+        {bookRankData && bookRankData.length > 0 ? (
+          <RankSwiper data={bookRankData} />
+        ) : (
+          <HaveNotData content={"베스트 셀러가"} />
+        )}
+      </div> */}
     </div>
   );
 };
