011b953358b602a7ac72ca8bdb43c69fad22d9b6
SeungHun6450 <toffg6450@naver.com>
2024-06-20 22:27:14 +0900

    design: global.css와 layout 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
 import { Footer } from "./components/Layout/Footer/Footer";
 import { Header } from "./components/Layout/Header/Header";
+import ReactQueryProvider from "./components/Provider/ReactQueryProvider/ReactQueryProvider";
+import ReduxProvider from "./components/Provider/ReduxProvider/ReduxProvider";
 import { GurmukhiMN, Inter, Kalufo, Pretendard, SpoqaHanSansNeo } from "./font";
 import "./globals.css";
-import ReduxProvider from "./components/Provider/ReduxProvider/ReduxProvider";
-import ReactQueryProvider from "./components/Provider/ReactQueryProvider/ReactQueryProvider";
 
 export const metadata: Metadata = {
   title: "지성인",
-  description: "책의 모든 것",
+  description: "책과 관련된 주제로 자유롭게 이야기를 나누는 곳",
 };
 
 export default function RootLayout({
@@ -25,7 +25,9 @@ export default function RootLayout({
           <ReactQueryProvider>
             <ReduxProvider>
               <Header />
-              <div className="flex flex-grow flex-col w-[100%]">{children}</div>
+              <div className="flex flex-col items-center w-[100%]">
+                {children}
+              </div>
               <Footer />
             </ReduxProvider>
           </ReactQueryProvider>
