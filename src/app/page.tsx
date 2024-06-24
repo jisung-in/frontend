d8310ed7257067fdc64c1821b5ff82b45e579ddb
SeungHun6450 <toffg6450@naver.com>
2024-06-24 15:25:56 +0900

    feat: 메인페이지에 베스트셀러 스와이퍼 추가, 스와이퍼 css 경계선 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import BestSeller from "@/assets/img/best-seller.svg";
 import CreateTalkRoom from "@/assets/img/create-talk-room.svg";
 import { useGetBookRank } from "@/hook/reactQuery/book/useGetBookRank";
 import { useGetMyDetail } from "@/hook/reactQuery/my/useGetMyDetail";
@@ -11,6 +12,10 @@ import { Button } from "./components/Button/Button";
 import TalkRoomCard from "./components/Card/MainPageCard/TalkRoomCard";
 import HaveNotData from "./components/HaveNotData/HaveNotData";
 import Pagination from "./components/Pagination/Pagination";
+import ResizeImage from "./components/ResizeImage/ResizeImage";
+import BestSellerSwiper from "./components/Swiper/BestSellerSwiper";
+import { ThemeMain } from "./components/Theme/Theme";
+
 type TalkRoom = {
   id: number;
   profileImage: string;
@@ -48,11 +53,7 @@ const page = () => {
   const { data: bookRankData } = useGetBookRank();
   return (
     <div className="flex flex-col items-center max-w-[1280px]">
-      <div
-        className="
-        xl2:mt-[85px] 
-        xl2:mb-[73px]"
-      >
+      <div className="mt-[85px] mb-[73px]">
         {/* 기록, 질문, 평가 컴포넌트 위치 */}
       </div>
 
@@ -96,28 +97,22 @@ const page = () => {
           link={currentUrl}
         />
       )}
-      {/* <div className=":my-[56px] px-[120px] ">
+
+      <div className="mb-[160px]">
         <ThemeMain.MainTheme>
-          <div
-            className="flex
-              sm:mb-[15px] md:mb-[17px] lg:mb-[19px] xl:mb-[19px] xl2:mb-[21px]"
-          >
-            <div
-              className="
-                flex grow items-center
-                sm:gap-x-1.5 md:gap-x-2 lg:gap-x-2.5 xl:gap-x-2.5 xl2:gap-x-3 "
-            >
+          <div className="flex mb-[21px]">
+            <div className="flex grow items-center gap-x-3 ">
               <div>지성인의 베스트 셀러</div>
               <ResizeImage src={BestSeller} alt="베스트 셀러" />
             </div>
           </div>
         </ThemeMain.MainTheme>
         {bookRankData && bookRankData.length > 0 ? (
-          <RankSwiper data={bookRankData} />
+          <BestSellerSwiper data={bookRankData} />
         ) : (
           <HaveNotData content={"베스트 셀러가"} />
         )}
-      </div> */}
+      </div>
     </div>
   );
 };
