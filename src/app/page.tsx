091b9088258aa1c8fa7a4841b2e4e1d09c7f86b7
SeungHun6450 <toffg6450@naver.com>
2024-07-02 16:46:04 +0900

    desing : 메인페이지 토크방 생성하기 버튼 gap-x 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ const page = () => {
           </div>
           <Link href={"/detail/talkroom/new"}>
             <Button className="w-[167px]" variant={"mainPage"} weight={"semi"}>
-              <div className="flex items-center gap-x-3 px-4">
+              <div className="flex items-center gap-x-2 px-4">
                 <CreateTalkRoom />
                 토크방 생성하기
               </div>
