0f41332360ca5e4fb90e871a167d1883a6d0f7d7
SeungHun6450 <toffg6450@naver.com>
2024-07-04 17:29:52 +0900

    feat : 스켈레톤ui 코드 간소화, 필요없는 부분 삭제, typescript 수정

--- src/app/components/MainSelectionCard/MainSelectionCard.tsx
+++ src/app/components/MainSelectionCard/MainSelectionCard.tsx
@@ -17,7 +17,7 @@ type MainSelectionCardType = {
     | "smallerQuestion"
     | "smallerEvaluation";
   rounded: boolean;
-  isLoading: boolean;
+  isLoading?: boolean;
 };
 
 const obj = {
@@ -83,12 +83,7 @@ const MainSelectionCard = ({
         style={{ backgroundColor: color }}
       >
         {isLoading ? (
-          <Skeleton className="rounded-[30px]">
-            <span className="flex w-full text-[40px] ml-9 mt-8 font-Jalnan2">
-              {text}
-            </span>
-            <div className="flex w-full justify-end pr-5 pb-2">{file}</div>
-          </Skeleton>
+          <Skeleton className="w-[413px] h-[270px] rounded-[30px]" />
         ) : (
           <>
             <span className="flex w-full text-[40px] ml-9 mt-8 font-Jalnan2">
@@ -111,41 +106,17 @@ const MainSelectionCard = ({
       )}
       style={{ backgroundColor: color }}
     >
-      {isLoading ? (
-        <Skeleton
-          className={clsx(
-            rounded
-              ? "rounded-[40px] gap-x-2 px-4 pt-[10px] pb-[11px]"
-              : "rounded-[13px] gap-x-3 px-4 py-3",
-          )}
-        >
-          <span
-            className={clsx(
-              "flex text-brown-60 font-Jalnan2",
-              type.slice(0, 1) === "m"
-                ? "w-[67px] text-[33px] pt-1.5"
-                : "w-[47px] text-[23px] pt-1.5",
-            )}
-          >
-            {text}
-          </span>
-          {file}
-        </Skeleton>
-      ) : (
-        <>
-          <span
-            className={clsx(
-              "flex text-brown-60 font-Jalnan2",
-              type.slice(0, 1) === "m"
-                ? "w-[67px] text-[33px] pt-1.5"
-                : "w-[47px] text-[23px] pt-1.5",
-            )}
-          >
-            {text}
-          </span>
-          {file}
-        </>
-      )}
+      <span
+        className={clsx(
+          "flex text-brown-60 font-Jalnan2",
+          type.slice(0, 1) === "m"
+            ? "w-[67px] text-[33px] pt-1.5"
+            : "w-[47px] text-[23px] pt-1.5",
+        )}
+      >
+        {text}
+      </span>
+      {file}
     </div>
   );
 };
