d335ee42e370de9067736383621d620e1c3a7be9
SeungHun6450 <toffg6450@naver.com>
2024-08-10 18:43:02 +0900

    feat: skeleton ui 적용을 위한 설치 및 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Footer } from "./components/Layout/Footer/Footer";
 import { Header } from "./components/Layout/Header/Header";
+import ReactQueryProvider from "./components/Provider/ReactQueryProvider/ReactQueryProvider";
+import ReduxProvider from "./components/Provider/ReduxProvider/ReduxProvider";
 import { GurmukhiMN, Inter, Kalufo, Pretendard, SpoqaHanSansNeo } from "./font";
 import "./globals.css";
-import ReduxProvider from "./components/Provider/ReduxProvider/ReduxProvider";
-import ReactQueryProvider from "./components/Provider/ReactQueryProvider/ReactQueryProvider";
-
+import { Providers } from "./providers";
 export const metadata: Metadata = {
   title: "지성인",
   description: "책의 모든 것",
@@ -24,9 +24,13 @@ export default function RootLayout({
         <div className="flex flex-col min-h-screen">
           <ReactQueryProvider>
             <ReduxProvider>
-              <Header />
-              <div className="flex flex-grow flex-col w-[100%]">{children}</div>
-              <Footer />
+              <Providers>
+                <Header />
+                <div className="flex flex-grow flex-col w-[100%]">
+                  {children}
+                </div>
+                <Footer />
+              </Providers>
             </ReduxProvider>
           </ReactQueryProvider>
         </div>
