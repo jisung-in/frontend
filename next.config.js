ec55c3f3e6a0964b02170e7d5af804e187906cc4
SeungHun6450 <toffg6450@naver.com>
2024-08-20 19:36:01 +0900

    feat: next.config.js의 images.domain에서 images.remotePatterns로 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,12 +1,44 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   images: {
-    domains: [
-      "k.kakaocdn.net",
-      "loremflickr.com",
-      "image.yes24.com",
-      "dummyimage.com",
-      "search1.kakaocdn.net",
-      "jisungin-bucket.s3.ap-northeast-2.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: isProd ? "https" : "http",
+        hostname: "k.kakaocdn.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "loremflickr.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "image.yes24.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: isProd ? "https" : "http",
+        hostname: "dummyimage.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "search1.kakaocdn.net",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "jisungin-bucket.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
     formats: ["image/avif", "image/webp"],
   },
