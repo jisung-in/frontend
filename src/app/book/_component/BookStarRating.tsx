96d650eff566aacedb34d678b806c9cb7d9cf402
SeungHun6450 <toffg6450@naver.com>
2024-12-22 01:20:18 +0900

    feat: 취소 메세지 컴포넌트 적용

--- src/app/book/_component/BookStarRating.tsx
+++ src/app/book/_component/BookStarRating.tsx
@@ -10,6 +10,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import debounce from "lodash.debounce";
 import dynamic from "next/dynamic";
 import { useCallback, useEffect, useState } from "react";
+import CancelStarRateMessage from "./CancelStarRateMessage";
 
 const Modal = dynamic(() => import("@/app/components/Modal/Modal"));
 
@@ -171,14 +172,7 @@ const BookStarRating = ({ isbn, ratingAverage }: BookStarRatingCondition) => {
                         onClick={() => clickStarRate(index, false)}
                         onMouseMove={() => mouseMove(index, false)}
                       />
-                      {cancelMessage(index) && (
-                        <>
-                          <span className="absolute w-[65px] text-center bottom-full left-1/2 transform -translate-x-1/2 bg-[#624E45] text-white text-sm px-2 py-1 rounded-md">
-                            취소하기
-                          </span>
-                          <div className="absolute w-0 h-0 border-8 border-solid border-[#624E45] border-t-[10px] border-r-transparent border-b-transparent border-l-transparent left-[70%] transform -translate-x-1/2" />
-                        </>
-                      )}
+                      {cancelMessage(index) && <CancelStarRateMessage />}
                       {myStarRate ? myStarRating(index) : noneMyStarRate(index)}
                     </div>
                   ))}
